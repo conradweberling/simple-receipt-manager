fdf1382ca6693c9e501b571fe2ac2915315a7736
Conrad Weberling <cw@weberling-solutions.de>
2021-01-10 22:07:52 +0100

    Add registration completed alert

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,7 +70,9 @@ class RegisterController extends Controller
 
         return $request->wantsJson()
             ? new JsonResponse([], 201)
-            : redirect($this->redirectPath());
+            : redirect($this->redirectPath())->with([
+                'status' => 'Registration was successfully completed.'
+            ]);
     }
 
     /**
