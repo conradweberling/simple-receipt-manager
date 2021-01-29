89b17db08588dd9645e871bd5c6dd7404232cf4f
Conrad Weberling <cw@weberling-solutions.de>
2021-01-29 22:37:02 +0100

    Add demo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Receipt;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Max',
+            'email' => 'max@conradweberling.com',
+            'password' => '$2y$10$tYzX7vNSUp7oml0n8q.aNuc6P3xxuJ.KOrLJbt7in2wpQm/CMmHYq'
+        ]);
+
+        User::factory(2)->create();
+
+        Receipt::factory(500)->create();
+
+        Cache::flush();
+
     }
 }
