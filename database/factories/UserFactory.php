ed89eca68ef055d3d642a4822d150546fd792eca
Conrad Weberling <cw@weberling-solutions.de>
2021-02-16 23:27:33 +0100

    Demo password adjusted

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->firstName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$17oQb9TW1gNaHqawehrjWu5gGYsz7JsgxmW8qHLmhLKEXimg0So46',
             'remember_token' => Str::random(10),
             'color' => $this->faker->hexColor
         ];
